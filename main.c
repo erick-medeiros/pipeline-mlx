d8aa8e6b2161946a28da8fa16f8866c5c3b52ac8
Erick Medeiros <erickmedeirosbr@gmail.com>
2023-02-08 20:52:05 -0300

    refactor: update main.c

--- main.c
+++ main.c
@@ -1,19 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: eandre-f <eandre-f@student.42sp.org.br>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/02/08 20:51:26 by eandre-f          #+#    #+#             */
+/*   Updated: 2023/02/08 20:51:45 by eandre-f         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <mlx.h>
 #include <stdlib.h>
 #include <unistd.h>
 
-#define WINDOW_W 100
-#define WINDOW_H 100
+#define WINDOW_WIDTH 100
+#define WINDOW_HEIGHT 100
 #define WINDOW_NAME "program"
 
-int main()
+int	main(void)
 {
-	void *mlx;
-	void *win;
+	void	*mlx;
+	void	*win;
 
 	mlx = mlx_init();
-	win = mlx_new_window(mlx, WINDOW_W, WINDOW_H, WINDOW_NAME);
-	// mlx_loop(mlx);
+	win = mlx_new_window(mlx, WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_NAME);
 	sleep(1);
 	mlx_destroy_window(mlx, win);
 	mlx_destroy_display(mlx);
